223efc76fe6c7279927208d66ce30aa37643707d
Dragoon <DragoonX6@users.noreply.github.com>
2022-05-26 00:02:03 +0200

    Reformat the test

--- ChiitransLiteTests/UnitTest1.cs
+++ ChiitransLiteTests/UnitTest1.cs
@@ -1,15 +1,18 @@
-﻿using System;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ChiitransLite.misc;
 
-namespace ChiitransLiteTests {
-    [TestClass]
-    public class UnitTest1 {
-        [TestMethod]
-        public void TestKataHira() {
-            string kata = "どー";
-            string hira = TextUtils.katakanaToHiragana(kata);
-            Assert.AreEqual("どう", hira);
-        }
-    }
+namespace ChiitransLiteTests
+{
+[TestClass]
+public class UnitTest1
+{
+	[TestMethod]
+	public void TestKataHira()
+	{
+		string kata = "どー";
+		string hira = TextUtils.katakanaToHiragana(kata);
+		Assert.AreEqual("どう", hira);
+	}
+}
 }
